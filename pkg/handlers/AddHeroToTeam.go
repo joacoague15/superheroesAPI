4775289c6a56e36d5cce8ebc88cfcfa14398c610
joacoague15 <joacoague15@gmail.com>
2022-10-27 02:16:05 +0100

    Finding a way to get userId

--- pkg/handlers/AddHeroToTeam.go
+++ pkg/handlers/AddHeroToTeam.go
@@ -8,20 +8,20 @@ import (
 	"superheroesAPI/pkg/models"
 )
 
-func (h Handler) AddHeroToTeam(w http.ResponseWriter, r *http.Request) {
-	userId, err := strconv.ParseInt(chi.URLParam(r, "userId"), 10, 64)
-	heroId, err := strconv.ParseInt(chi.URLParam(r, "heroId"), 10, 64)
+func (h Handler) AddHeroToTeam(_ http.ResponseWriter, r *http.Request) {
+	heroId, err := strconv.ParseInt(chi.URLParam(r, "id"), 10, 64)
 
 	if err != nil {
 		log.Println(err)
 	}
 
 	teamMember := models.TeamMember{
-		UserId: userId,
+		// HARDCODED
+		UserId: 8,
 		HeroId: heroId,
 	}
 
-	tx := h.DB.Table("teamMembers").Create(&teamMember)
+	tx := h.DB.Table("teamMembers").Debug().Create(&teamMember)
 	if tx.Error != nil {
 		log.Println(tx.Error)
 	}
